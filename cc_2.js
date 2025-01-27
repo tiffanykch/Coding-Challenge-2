89635cc70f3a62455e24bcf5369e85b94e3ff5e3
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-01-26 22:24:18 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -14,3 +14,21 @@ products.pop()
 
 //Display final array to the console
 console.log("Final Inventory:", products)
+
+//TASK 2: ACCESSING AND MODIFYING ARRAYS
+
+//Creating an array containing at least 5 numerical values
+let scores = [15, 45, 52, 90, 100, 89]
+console.log("Initial Scores:", scores)
+
+//Update second score in array
+scores[1] = 75
+
+//Calculate the average of all scores
+let total = scores.reduce((sum, score) => sum + score, 0)
+let average = total/scores.length
+
+//Display output to the console
+console.log("Updated Scores:", scores)
+console.log("Average Score:", average.toFixed(2))
+
