0c5fd5d87397db9b59de0e1a12f5cfba33dd041a
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-01-26 22:12:23 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,16 @@
+//TASK 1: WORKING WITH ARRAYS - STORE INVENTORY SYSTEM
+
+//Creating an array containing at least 5 products
+let products = ["Tomatoes", "Cabbage", "Carrots", "Eggs", "Cilantro"]
+
+//Adding new product to the array
+products.push("Maruchan")
+
+//Display new array to the console
+console.log("New Inventory:", products)
+
+//Remove last product from the array
+products.pop()
+
+//Display final array to the console
+console.log("Final Inventory:", products)
